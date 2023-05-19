a798d85b49eec4dc62988d444e1f66d5ddddd230
chury <chury99@gmail.com>
2023-05-19 17:21:18 +0900

    6개번호 5세트 선정 - 비어있는 날짜 데이터 자동 생성

--- m30_selector.py
+++ m30_selector.py
@@ -8,7 +8,7 @@ import m31_selector_algorithm as logic
 
 # noinspection PyUnresolvedReferences,PyPep8Naming,PyProtectedMember
 class Selector:
-    def __init__(self, s_기준일=None, b_test=False):
+    def __init__(self):
         # config 읽어 오기
         with open('config.json', mode='rt', encoding='utf-8') as file:
             dic_config = json.load(file)
@@ -16,8 +16,6 @@ class Selector:
         # 기준 정보 설정
         self.s_오늘 = pd.Timestamp('now').strftime('%Y%m%d')
         self.path_log = os.path.join(dic_config['folder_log'], f'log_lotto_{self.s_오늘}.log')
-        self.s_기준일 = self.s_오늘 if s_기준일 is None else s_기준일
-        self.b_test = b_test
 
         # 폴더 설정
         folder_work = dic_config['folder_work']
@@ -44,42 +42,59 @@ class Selector:
         # import API_kakao
 
         # log 기록
-        self.make_log(f'### 번호 선정 시작 (기준일-{self.s_기준일}) ###')
+        self.make_log(f'### 번호 선정 시작 ###')
 
-        # 예측 기준정보 생성
-        self.n_예측_차수, self.s_예측_추첨일 = self._예측기준정보생성(s_기준일=self.s_기준일)
-
-        # 확률값 읽어오기
-        self.dic_확률 = self._확률값_읽어오기()
+        # 회차별 추첨일 데이터 생성
+        self.dic_추첨일 = self._회차별추첨일생성()
 
     def 번호선정(self, s_선정로직):
-        # 로그 기록
-        self.make_log(f'# {s_선정로직} ({self.n_예측_차수:,}차_{self.s_예측_추첨일}추첨) #')
+        # 대상 회차 선정 (6개 확률 완료된 회차 찾기)
+        s_폴더_확인대상 = os.path.join(self.folder_확률예측)
+        li_파일명 = [s_파일명 for s_파일명 in os.listdir(s_폴더_확인대상)
+                  if '확률예측_6개번호_' in s_파일명 and '.csv' in s_파일명 and 'summary_' not in s_파일명]
+        li_정보 = [s_파일명.split(sep='_') for s_파일명 in li_파일명]
+        li_회차_대상 = [int(s_정보[2].replace('차', '')) for s_정보 in li_정보]
 
-        # 번호 선정
-        s_전략명 = None
-        df_6개번호_5개세트 = None
-        dic_확률 = self.dic_확률
-        if s_선정로직 == '번호선정_2개번호_최빈수':
-            s_전략명, df_6개번호_5개세트 = logic.번호선정로직_2개번호_최빈수(dic_확률=dic_확률)
-        if s_선정로직 == '번호선정_2개번호_최빈수_중복제외':
-            s_전략명, df_6개번호_5개세트 = logic.번호선정로직_2개번호_최빈수_중복제외(dic_확률=dic_확률)
-        if s_선정로직 == '번호선정_2개번호_따라가기':
-            s_전략명, df_6개번호_5개세트 = logic.번호선정로직_2개번호_따라가기(dic_확률=dic_확률)
-        if s_선정로직 == '번호선정_2개번호_따라가기_확률반영':
-            s_전략명, df_6개번호_5개세트 = logic.번호선정로직_2개번호_따라가기_확률반영(dic_확률=dic_확률)
-        if s_선정로직 == '번호선정_복합로직_최빈수_따라가기':
-            s_전략명, df_6개번호_5개세트 = logic.번호선정로직_복합로직_최빈수_따라가기(dic_확률=dic_확률)
-        if s_선정로직 == '번호선정_1개2개연계_최빈수연계':
-            s_전략명, df_6개번호_5개세트 = logic.번호선정로직_1개2개연계_최빈수연계(dic_확률=dic_확률)
+        # 진행할 회차 선정 (완료된 파일 확인해서 새로운 회차만 진행)
+        s_폴더_선정결과 = os.path.join(self.folder_번호선정, f'번호선정로직_{s_선정로직[5:]}')
+        os.makedirs(s_폴더_선정결과, exist_ok=True)
+        li_파일명 = [s_파일명 for s_파일명 in os.listdir(s_폴더_선정결과)
+                  if '확률예측_번호선정_' in s_파일명 and '.csv' in s_파일명 and 'summary_' not in s_파일명]
+        li_정보 = [s_파일명.split(sep='_') for s_파일명 in li_파일명]
+        li_회차_완료 = [int(s_정보[2].replace('차', '')) for s_정보 in li_정보]
 
-        # 결과 저장용 폴더 생성
-        folder_전략명 = os.path.join(self.folder_번호선정, s_전략명)
-        os.makedirs(folder_전략명, exist_ok=True)
+        li_회차_할거 = [n_회차 for n_회차 in li_회차_대상 if n_회차 not in li_회차_완료]
 
-        # csv 저장
-        s_파일명 = f'확률예측_번호선정_{self.n_예측_차수}차_{self.s_예측_추첨일}추첨.csv'
-        df_6개번호_5개세트.to_csv(os.path.join(folder_전략명, s_파일명), index=False)
+        for n_회차 in li_회차_할거:
+            s_추첨일 = self.dic_추첨일[n_회차]
+
+            # 로그 기록
+            self.make_log(f'# {s_선정로직} ({n_회차:,}차_{s_추첨일}추첨) #')
+
+            # 번호 선정
+            s_전략명 = None
+            df_6개번호_5개세트 = None
+            dic_확률 = self._확률값_읽어오기(n_예측_차수=n_회차, s_예측_추첨일=s_추첨일)
+            if s_선정로직 == '번호선정_2개번호_최빈수':
+                s_전략명, df_6개번호_5개세트 = logic.번호선정로직_2개번호_최빈수(dic_확률=dic_확률)
+            if s_선정로직 == '번호선정_2개번호_최빈수_중복제외':
+                s_전략명, df_6개번호_5개세트 = logic.번호선정로직_2개번호_최빈수_중복제외(dic_확률=dic_확률)
+            if s_선정로직 == '번호선정_2개번호_따라가기':
+                s_전략명, df_6개번호_5개세트 = logic.번호선정로직_2개번호_따라가기(dic_확률=dic_확률)
+            if s_선정로직 == '번호선정_2개번호_따라가기_확률반영':
+                s_전략명, df_6개번호_5개세트 = logic.번호선정로직_2개번호_따라가기_확률반영(dic_확률=dic_확률)
+            if s_선정로직 == '번호선정_복합로직_최빈수_따라가기':
+                s_전략명, df_6개번호_5개세트 = logic.번호선정로직_복합로직_최빈수_따라가기(dic_확률=dic_확률)
+            if s_선정로직 == '번호선정_1개2개연계_최빈수연계':
+                s_전략명, df_6개번호_5개세트 = logic.번호선정로직_1개2개연계_최빈수연계(dic_확률=dic_확률)
+
+            # 결과 저장용 폴더 생성
+            folder_전략명 = os.path.join(self.folder_번호선정, s_전략명)
+            os.makedirs(folder_전략명, exist_ok=True)
+
+            # csv 저장
+            s_파일명 = f'확률예측_번호선정_{n_회차}차_{s_추첨일}추첨.csv'
+            df_6개번호_5개세트.to_csv(os.path.join(folder_전략명, s_파일명), index=False)
 
     ###################################################################################################################
 
@@ -101,12 +116,8 @@ class Selector:
             with open(self.path_log, mode='at', encoding='cp949') as file:
                 file.write(f'{s_log}\n')
 
-    def _확률값_읽어오기(self):
+    def _확률값_읽어오기(self, n_예측_차수, s_예측_추첨일):
         """ 최종 구매할 6개 번호 * 5개 세트 선정 후 result 폴더에 csv 저장 (기존 데이터는 csv로 읽어 오기) """
-        # 예측 기준정보 정의
-        n_예측_차수 = self.n_예측_차수
-        s_예측_추첨일 = self.s_예측_추첨일
-
         # 확률 분석 결과 가져오기
         dic_확률 = dict()
         s_파일명 = f'확률예측_1개번호_{n_예측_차수}차_{s_예측_추첨일}추첨.csv'
@@ -115,54 +126,37 @@ class Selector:
         dic_확률['df_확률_2개'] = pd.read_csv(os.path.join(self.folder_확률예측, s_파일명), encoding='cp949')
         s_파일명 = f'확률예측_6개번호_{n_예측_차수}차_{s_예측_추첨일}추첨.csv'
         dic_확률['df_확률_6개'] = pd.read_csv(os.path.join(self.folder_확률예측, s_파일명), encoding='cp949')
-        # df_확률_6개 = pd.read_csv(os.path.join(self.folder_확률예측, s_파일명), encoding='cp949')
-        # for n in range(6):
-        #     df_확률_6개[f'no{n + 1}'] = df_확률_6개[f'no{n + 1}'].apply(lambda x: f'{x:02}')
-        # dic_확률['df_확률_6개'] = df_확률_6개
 
         return dic_확률
 
-    def _예측기준정보생성(self, s_기준일):
-        """ 예측 차수, 예측 추첨일 정보 생성 후 리턴 """
-        # 이력 데이터 가져오기
-        df_이력 = pd.read_csv(os.path.join(self.folder_history, 'lotto_history.csv'), encoding='cp949').astype(str)
-        df_이력 = df_이력.sort_values('추첨일').reset_index(drop=True)
+    def _회차별추첨일생성(self):
+        """ 이력 데이터 불러와서 다음 회차 추가한 후 회차별 추첨일 데이터 생성 후 dict 리턴 """
+        # 이력 데이터 불러오기
+        df_이력 = pd.read_csv(os.path.join(self.folder_history, 'lotto_history.csv'), encoding='cp949')
+        df_이력['추첨일'] = df_이력['추첨일'].apply(lambda x: x.replace('.', ''))
 
-        # 예측용 데이터 준비
-        s_기준일 = pd.Timestamp(s_기준일).strftime('%Y.%m.%d')
-        df_데이터 = df_이력[df_이력['추첨일'] <= s_기준일]
-        # df_데이터 = df_데이터[-1:]
+        # dic 생성
+        dic_추첨일 = df_이력.set_index('회차').to_dict()['추첨일']
 
-        # 예측 기준정보 생성
-        n_예측_차수 = int(df_데이터['회차'].values[-1]) + 1
-        dt_예측_추첨일 = pd.Timestamp(df_데이터['추첨일'].values[-1]) + pd.Timedelta(days=7)
-        s_예측_추첨일 = dt_예측_추첨일.strftime('%Y%m%d')
+        # 다음 회차 데이터 생성
+        n_회차_마지막 = df_이력['회차'].max()
+        n_회차_다음 = n_회차_마지막 + 1
+        dt_추첨일_다음 = pd.Timestamp(dic_추첨일[n_회차_마지막]) + pd.Timedelta(days=7)
+        n_추첨일_다음 = dt_추첨일_다음.strftime('%Y%m%d')
 
-        return n_예측_차수, s_예측_추첨일
+        # 다음 회차 데이터 추가
+        dic_추첨일[n_회차_다음] = n_추첨일_다음
+
+        return dic_추첨일
 
 
 #######################################################################################################################
 if __name__ == '__main__':
-    # 기준일 없으면 오늘 날짜, b_test 없으면 False
-    # s = Selector(s_기준일='20230331', b_test=True)
-    s = Selector(s_기준일=None)
+    # 6개 확률 데이터 기준 빈 날짜 자동 생성
+    s = Selector()
     s.번호선정(s_선정로직='번호선정_2개번호_최빈수')
     s.번호선정(s_선정로직='번호선정_2개번호_최빈수_중복제외')
     s.번호선정(s_선정로직='번호선정_2개번호_따라가기')
     s.번호선정(s_선정로직='번호선정_2개번호_따라가기_확률반영')
     s.번호선정(s_선정로직='번호선정_복합로직_최빈수_따라가기')
     s.번호선정(s_선정로직='번호선정_1개2개연계_최빈수연계')
-
-    # 과거 데이터 생성용 실행 코드
-    # li_기준일 = [pd.Timestamp('20180105') + pd.Timedelta(days=7*n) for n in range(281)]
-    # li_기준일 = [dt.strftime('%Y%m%d') for dt in li_기준일]
-    #
-    # from tqdm import tqdm
-    # for s_기준일 in tqdm(li_기준일):
-    #     s = Selector(s_기준일=s_기준일)
-    #     s.번호선정(s_선정로직='번호선정_2개번호_최빈수')
-    #     s.번호선정(s_선정로직='번호선정_2개번호_최빈수_중복제외')
-    #     s.번호선정(s_선정로직='번호선정_2개번호_따라가기')
-    #     s.번호선정(s_선정로직='번호선정_2개번호_따라가기_확률반영')
-    #     s.번호선정(s_선정로직='번호선정_복합로직_최빈수_따라가기')
-    #     s.번호선정(s_선정로직='번호선정_1개2개연계_최빈수연계')
