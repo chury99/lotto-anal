e25ebf22350d598308504b37941f9d27e18e188f
chury <chury99@gmail.com>
2023-05-20 14:34:24 +0900

    당첨결과 확인 - 카카오톡 연계

--- m40_verifier.py
+++ m40_verifier.py
@@ -38,10 +38,6 @@ class Verifier:
         else:
             self.dic_args = dict()
 
-        # 카카오 API 연결
-        # sys.path.extend(dic_config['folder_kakao'])
-        # import API_kakao
-
         # log 기록
         self.make_log(f'### 결과 확인 시작 ###')
 
